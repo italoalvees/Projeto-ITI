3dbed2aa7b7e56edaa9b1c884b6b498b8d459292
italoalvees <italo_alvees@live.com>
2021-05-25 15:20:07 -0300

    Comentários adicionados ao código compressor.py

--- compressor.py
+++ compressor.py
@@ -3,21 +3,38 @@ from typing import AnyStr
 import time
 import timeit
 
+#Iniciar os dois dicionários usados no compressor e descompressor LZW, 
+#são utilizados os valores da tabela ASCII como alfabeto inicial.
+#No primeiro é utilizado o valor em Bytes para o indice e no segundo isso é invertido
+
 ASCII_para_INT: dict = {i.to_bytes(1, 'big'): i for i in range(256)}
 INT_para_ASCII: dict = {i: b for b, i in ASCII_para_INT.items()}
 
+
+#Função codificar recebe uma mensagem em qualquer formato e o número de bits que serão usados como indices
 def codificar(mensagem: AnyStr, tamanhoKbits: int) -> bytes:
     
     if isinstance(mensagem, str):
         mensagem = mensagem.encode()
     
     print('Tamanho Mensagem Descompactada: ', len(mensagem))
+    
+    
+    #Chaves: dicionário inicial / numero_de_chaves: quantidade de valores no dicionário
+    #codificado: Vetor vazio que vai receber os valores codificados/ tam_mensagem: variável usada como fim do loop
+    # inicio: é usado como variável principal para saber onde se iniciará as combinações, após serem adicionadas no dicionário
     chaves: dict = ASCII_para_INT.copy()
     numero_de_chaves: int = len(chaves)
     codificado: list = []
     inicio: int = 0
     tam_mensagem: int = len(mensagem) + 1
     
+
+    #Corpo principal da função, inicialmente é testado se o dicionário está preenchido para poder reseta-lo,
+    #após isso é iniciado um for que irá de 1 até o tam_mensagem - inicio, esse valor corresponde ao tamanho inicial da msg
+    #  - onde está o começo das combinações restantes. Dentro desse for é criada a combinação [inicio : inicio + 1], caso esteja no dicionário,
+    #é começado um novo loop e incrementado o i, aumentando assim um caractere na combinação, esse loop só para quando acha uma combinação que 
+    # não esteja no dicionário, ai é adicionado no dicionário e resetado o while, seu ponto de parada é ao acabar todas os caracteres da msg.
     while True:
 
         if numero_de_chaves >= 2**tamanhoKbits:
@@ -37,17 +54,26 @@ def codificar(mensagem: AnyStr, tamanhoKbits: int) -> bytes:
             codificado.append(chaves[palavra])
             break
     
+    #Aqui é concatenado todos os valores codificados e são preenchidos para terem o tamanhoKbits de bits, 
+    #formando assim o arquivo final que será armazenado 
     bits: str = ''.join([bin(i)[2:].zfill(tamanhoKbits) for i in codificado])
         
     return int(bits, 2).to_bytes(ceil(len(bits) / 8), 'big')
 
 
+
+#Função decodificar recebe uma mensagem em qualquer formato e o número de bits que serão usados como indices
 def decodificar(mensagem: AnyStr, tamanhoKbits: int) -> bytes:
     
     if isinstance(mensagem, str):
         mensagem = mensagem.encode()
    
     print('Tamanho Mensagem Compactada: ', len(mensagem)) 
+
+    #Chaves: dicionário inicial / bits: mensagem codificada em bits
+    #cn_extended_bytes: quantidade de caracteres existentes na mensagem / bits²: a mensagem colocada em seu tamanho total, eliminando os 0's excedentes
+    # mensagem_lista: são retirados os valores codificados no tamanhoKbits, para serem decodificados
+    #anterior: a combinação anterior / descodificado: lista final dos valores decodificados/ numero_de_chaves: tamanho do dicionário
 
     chaves: dict = INT_para_ASCII.copy()
     bits: str = bin(int.from_bytes(mensagem, 'big'))[2:].zfill(len(mensagem) * 8)
@@ -59,6 +85,14 @@ def decodificar(mensagem: AnyStr, tamanhoKbits: int) -> bytes:
     anterior: bytes = chaves[mensagem_lista[0]]
     descodificado: list = [anterior]
     numero_de_chaves: int = len(chaves)
+    
+
+
+    #Esse é o corpo principal da função, onde são lidas as msgs codificadas uma por uma,
+    #primeiro é testado se o dicionário está cheio, o mesmo método que é usado no codificador para que não haja erros,
+    # depois é procurado o valor correspondente no dicionário, caso não pertença ao dicionário atual, 
+    # é inferido que o valor seja a combinação anterior mais ela mesma. O valor decodificado é adicionado na string final 
+    # e é adicionado a combinação dentro do dicionário, e também incrementado a variável de marcação assim como é guardado o valor atual.
     
     for i in mensagem_lista[1:]:
 
@@ -76,6 +110,7 @@ def decodificar(mensagem: AnyStr, tamanhoKbits: int) -> bytes:
         anterior = atual
         numero_de_chaves += 1
 
+    #Por fim é retornado o arquivo decoficado em bytes.
     return (b''.join(descodificado)).strip()
 
 arquivos = ['video/video.mp4','texto/corpus16MB.txt']    
